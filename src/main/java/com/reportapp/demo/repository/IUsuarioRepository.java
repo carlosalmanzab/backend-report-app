f2e54059e60a479a411b23742ca3a21c3efd4cfd
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-10-16 02:02:01 -0500

    agregar Uusario repository

--- src/main/java/com/reportapp/demo/repository/IUsuarioRepository.java
+++ src/main/java/com/reportapp/demo/repository/IUsuarioRepository.java
@@ -1,18 +1,23 @@
 package com.reportapp.demo.repository;
 
-import com.reportapp.demo.model.Usuario;
+import com.reportapp.demo.entity.Reporte;
+import com.reportapp.demo.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Usuario findByEmailAndPassword(String email, String password);
+    Optional<List<Reporte>> findAllById(Long id);
+    Optional<List<Reporte>> findAllByIdentificacion(String identificacion);
+    Usuario save(Usuario usuario);
+    Optional<Usuario> findByEmailAndPassword(String email, String password);
+    Optional<Boolean> existsByEmail(String email);
+    Optional<Boolean> existsByEmailAndPassword(String email, String password);
+    Optional<Boolean> existsByIdentificacion(String email);
 
-    Boolean existsByEmail(String email);
-
-    Boolean existsByEmailAndPassword(String email, String password);
-
-    Boolean existsByIdentificacion(String email);
 }
