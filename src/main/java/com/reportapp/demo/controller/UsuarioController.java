20c47223714f1a64164450cb62eb6ea4ccbdd552
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-10-16 02:03:45 -0500

    agregar Controller de Usuario

--- src/main/java/com/reportapp/demo/controller/UsuarioController.java
+++ src/main/java/com/reportapp/demo/controller/UsuarioController.java
@@ -1,19 +1,12 @@
 package com.reportapp.demo.controller;
 
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTOSave;
 import com.reportapp.demo.service.UsuarioService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -23,27 +16,24 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping("/{identificacion}")
-    @Operation(
-            summary = "Buscar usuario por documento de identificación",
-            description = "Buscar usuario por documento de identificación",
-            tags = {"Usuario"})
-    public ResponseEntity<?> existe(@PathVariable String identificacion) {
-        boolean exists = false;
-        Map<String, Object> response = new HashMap<>();
-
-        try {
-            exists = usuarioService.verificarIdentificacion(identificacion);
-        } catch (DataAccessException e) {
-            response.put("mensaje", "Error al verificar la identificación");
-            response.put("error", e);
-
-            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        response.put("mensaje", "El usuario existe en la BD");
-        response.put("usuarioExiste", exists);
-
-        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        return usuarioService.buscarPorId(id);
     }
+
+    @PostMapping()
+    public ResponseEntity<?> guardar(@Valid @RequestBody UsuarioDTOSave usuarioDTOSave) {
+        return usuarioService.registrar(usuarioDTOSave);
+    }
+
+    @GetMapping("/{id}/reportes")
+    public ResponseEntity<?> reportesPorId(@PathVariable Long id) {
+        return usuarioService.reportesPorId(id);
+    }
+
+    @GetMapping("/{identificacion}/reportes")
+    public ResponseEntity<?> reportesPorIdentificacion(@PathVariable String identificacion) {
+        return usuarioService.reportesPorIdentificacion(identificacion);
+    }
+
 }
