726eaa5bef524eb79c01091b454baeb64e764e5c
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-10-16 02:02:17 -0500

    agregar Usuario service

--- src/main/java/com/reportapp/demo/service/UsuarioService.java
+++ src/main/java/com/reportapp/demo/service/UsuarioService.java
@@ -1,15 +1,21 @@
 package com.reportapp.demo.service;
 
-import com.reportapp.demo.model.Usuario;
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTOLogin;
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTOSave;
+import org.springframework.http.ResponseEntity;
 
 public interface UsuarioService {
 
-    boolean registrar(Usuario usuario);
+    ResponseEntity<?> buscarPorId(Long id);
+    ResponseEntity<?> registrar(UsuarioDTOSave usuarioDTOSave);
+    ResponseEntity<?> login(UsuarioDTOLogin usuarioDTOLogin);
+    ResponseEntity<?> reportesPorId(Long id);
+    ResponseEntity<?> reportesPorIdentificacion(String identificacion);
+/*
+    ResponseMessage verificarExistencia(Usuario usuario) throws UsuarioNotFoundException;
 
-    boolean verificarExistencia(Usuario usuario);
+    UsuarioDTO loguin();
 
-    Usuario loguin(String email, String password);
-
-    boolean verificarIdentificacion(String identificacion);
-
+    ResponseMessage verificarIdentificacion(String identificacion) throws UsuarioNotFoundException;
+*/
 }
--- src/main/java/com/reportapp/demo/service/serviceImpl/UsuarioServiceImpl.java
+++ src/main/java/com/reportapp/demo/service/serviceImpl/UsuarioServiceImpl.java
@@ -1,10 +1,26 @@
 package com.reportapp.demo.service.serviceImpl;
 
-import com.reportapp.demo.model.Usuario;
+import com.reportapp.demo.entity.Reporte;
+import com.reportapp.demo.entity.dto.reporte.ReporteDTO;
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTOLogin;
+import com.reportapp.demo.entity.mapper.ReporteMapper;
+import com.reportapp.demo.entity.mapper.UsuarioMapper;
+import com.reportapp.demo.share.constant.UsuarioMessageConstants;
+import com.reportapp.demo.share.dto.ResponseMessage;
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTO;
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTOSave;
+import com.reportapp.demo.entity.Usuario;
 import com.reportapp.demo.repository.IUsuarioRepository;
 import com.reportapp.demo.service.UsuarioService;
+import com.reportapp.demo.util.PasswordEncoder;
+import jakarta.persistence.Cacheable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -12,18 +28,125 @@ public class UsuarioServiceImpl implements UsuarioService {
     @Autowired
     private IUsuarioRepository usuarioRepository;
 
-    @Override
-    public boolean registrar(Usuario usuario) {
-        return usuarioRepository.save(usuario) != null;
+    private UsuarioMapper usuarioMapper;
+
+    private ReporteMapper reporteMapper;
+
+    public UsuarioServiceImpl(UsuarioMapper usuarioMapper, ReporteMapper reporteMapper) {
+        this.usuarioMapper = usuarioMapper;
+        this.reporteMapper = reporteMapper;
     }
 
     @Override
-    public boolean verificarExistencia(Usuario usuario) {
+    public ResponseEntity<?> buscarPorId(Long id) {
+        ResponseMessage.ResponseMessageBuilder responseMessage =  ResponseMessage.builder();
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()) {
+            responseMessage
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_NO_FOUND)
+                    .build();
+            return new ResponseEntity<>(responseMessage, HttpStatus.NOT_FOUND);
+        }
+        UsuarioDTO usuarioDTO = usuarioMapper.toDTOWithReportes(usuario.get());
+        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> registrar(UsuarioDTOSave usuarioDTOSave) {
+        ResponseMessage.ResponseMessageBuilder responseMessage =  ResponseMessage.builder();
+
+        if (usuarioDTOSave == null) {
+            responseMessage
+                    .code(HttpStatus.BAD_REQUEST.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_NULL)
+                    .build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMessage);
+        }
+
+        Usuario usuario = usuarioMapper.toEntity(usuarioDTOSave);
+        usuario.setPassword(PasswordEncoder.encode(usuarioDTOSave.getPassword()));
+        Usuario usuarioSave = usuarioRepository.save(usuario);
+
+        if (usuarioSave == null) {
+            responseMessage
+                    .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_SAVE)
+                    .build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+        }
+
+        responseMessage
+                .code(HttpStatus.CREATED.value())
+                .message(UsuarioMessageConstants.USUARIO_SUCCESS_SAVE)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
+    }
+
+    @Override
+    public ResponseEntity<?> login(UsuarioDTOLogin usuarioDTOLogin) {
+        ResponseMessage.ResponseMessageBuilder responseMessage = ResponseMessage.builder();
+
+        if (usuarioDTOLogin == null) {
+            responseMessage
+                    .code(HttpStatus.BAD_REQUEST.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_NULL)
+                    .build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMessage);
+        }
+
+        Optional<Usuario> usuario = usuarioRepository.findByEmailAndPassword(usuarioDTOLogin.getEmail(), usuarioDTOLogin.getEmail());
+
+        if (usuario.isEmpty()) {
+            responseMessage
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_NO_FOUND)
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMessage);
+        }
+
+        UsuarioDTO usuarioDTO = usuarioMapper.toDTO(usuario.get());
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioDTO);
+    }
+
+    @Override
+    public ResponseEntity<?> reportesPorId(Long id) {
+        ResponseMessage.ResponseMessageBuilder responseMessage = ResponseMessage.builder();
+        Optional<List<Reporte>> reportes = usuarioRepository.findAllById(id);
+        if (reportes.isEmpty()) {
+            responseMessage
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_NO_FOUND_REPORTES)
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMessage);
+        }
+        List<ReporteDTO> reporteDTO = reporteMapper.toReporteDTOList(reportes.get());
+        return ResponseEntity.status(HttpStatus.OK).body(reporteDTO);
+    }
+
+    @Override
+    public ResponseEntity<?> reportesPorIdentificacion(String identificacion) {
+        ResponseMessage.ResponseMessageBuilder responseMessage = ResponseMessage.builder();
+        Optional<List<Reporte>> reportes = usuarioRepository.findAllByIdentificacion(identificacion);
+        if (reportes.isEmpty()) {
+            responseMessage
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(UsuarioMessageConstants.USUARIO_ERROR_NO_FOUND_REPORTES)
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMessage);
+        }
+        List<ReporteDTO> reporteDTO = reporteMapper.toReporteDTOList(reportes.get());
+        return ResponseEntity.status(HttpStatus.OK).body(reporteDTO);
+    }
+/*
+    @Override
+    public boolean verificarExistencia(Usuario usuario) throws UsuarioNotFoundException {
         return usuarioRepository.existsByEmail(usuario.getEmail());
     }
 
     @Override
-    public Usuario loguin(String email, String password) {
+    public Usuario loguin(String email, String password) throws UsuarioNotFoundException {
         boolean exists = usuarioRepository.existsByEmailAndPassword(email, password);
 
         if (exists) {
@@ -35,8 +158,8 @@ public class UsuarioServiceImpl implements UsuarioService {
     }
 
     @Override
-    public boolean verificarIdentificacion(String identificacion) {
+    public boolean verificarIdentificacion(String identificacion) throws UsuarioNotFoundException {
         return usuarioRepository.existsByIdentificacion(identificacion) != null;
     }
-
+*/
 }
