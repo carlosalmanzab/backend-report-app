f985e4ac64b1eeefc1262a0e9d747361823899f4
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-10-16 22:23:37 -0500

    agregar autentifiacacion

---
+++ src/main/java/com/reportapp/demo/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.reportapp.demo.controller;
+
+import com.reportapp.demo.entity.dto.usuario.UsuarioDTOLogin;
+import com.reportapp.demo.service.UsuarioService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@Tag(name = "Auth", description = "Endpoint para crear y manejar las sesiones.")
+public class AuthController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/loguin")
+    public ResponseEntity<?> login(@RequestBody UsuarioDTOLogin usuarioDTOLogin) {
+        return usuarioService.login(usuarioDTOLogin);
+    }
+
+    /*
+    @PostMapping("/register")
+    public ResponseEntity<?> login(@RequestBody UsuarioDTOLogin usuarioDTOLogin) {
+        return usuarioService.loguin(usuarioDTOLogin);
+    }*/
+
+}
