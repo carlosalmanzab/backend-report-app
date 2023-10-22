50febce6686f833fb8b54c53526623f54272e7a1
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-10-22 00:59:52 -0500

    eliminar constantes en desuso

--- src/main/java/com/reportapp/demo/share/constant/TipoReporteMessageConstants.java
+++
@@ -1,9 +0,0 @@
-package com.reportapp.demo.share.constant;
-
-public class TipoReporteMessageConstants {
-    public static final String TIPO_REPORTE_SUCCESS_SAVE = "El Tipo Reporte fue guardado con exito";
-    public static final String TIPO_REPORTE_ERROR_NO_FOUND = "Tipo Reporte no Encontrado";
-    public static final String TIPO_REPORTE_ERROR_NULL = "Tipo Reporte con valor Null";
-    public static final String TIPO_REPORTE_ERROR_SAVE = "Error al guardar el Tipo Reporte";
-    public static final String TIPO_REPORTE_ERROR_NO_FOUND_TIPO_REPORTES = "No se encontraron Tipos de Reportes";
-}
