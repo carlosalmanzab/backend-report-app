e9f59d586bc3627be1c0aa275159b0149734e45e
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-10-29 17:12:21 -0500

    agregar Swagger definition de la api

--- src/main/java/com/reportapp/demo/BackendReportAppApplication.java
+++ src/main/java/com/reportapp/demo/BackendReportAppApplication.java
@@ -3,8 +3,13 @@ package com.reportapp.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Report API", version = "1.0", description = "Report management API"))
 public class BackendReportAppApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BackendReportAppApplication.class, args);
