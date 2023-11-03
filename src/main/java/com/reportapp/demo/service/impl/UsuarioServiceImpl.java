460d1b13bf3460cded0854b6cf2ca8ab5b51c0aa
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2023-11-02 23:13:33 -0500

    eliminar comentarios

--- src/main/java/com/reportapp/demo/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/reportapp/demo/service/impl/UsuarioServiceImpl.java
@@ -1,14 +1,9 @@
 package com.reportapp.demo.service.impl;
 
-import com.reportapp.demo.entity.Barrio;
-import com.reportapp.demo.entity.Reporte;
 import com.reportapp.demo.entity.dto.reporte.ReporteDTO;
-import com.reportapp.demo.entity.dto.usuario.UsuarioDTOLogin;
-import com.reportapp.demo.entity.mapper.ReporteMapper;
 import com.reportapp.demo.entity.mapper.UsuarioMapper;
 import com.reportapp.demo.entity.usuario.Usuario;
 import com.reportapp.demo.entity.dto.usuario.UsuarioDTO;
-import com.reportapp.demo.entity.dto.usuario.UsuarioDTOSave;
 import com.reportapp.demo.repository.IUsuarioRepository;
 import com.reportapp.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,29 +43,4 @@ public class UsuarioServiceImpl implements UsuarioService {
         return ResponseEntity.ok(usuarioDTO);
     }
 
-/*
-
-    @Override
-    public Usuario loguin(String email, String password) throws UsuarioNotFoundException {
-        boolean exists = usuarioRepository.existsByEmailAndPassword(email, password);
-
-        if (exists) {
-            return usuarioRepository.findByEmailAndPassword(email, password);
-        } else {
-            return null;
-        }
-
-    }
-
-    @Override
-    public boolean verificarIdentificacion(String identificacion) throws UsuarioNotFoundException {
-        return usuarioRepository.existsByIdentificacion(identificacion) != null;
-    }
-*/
-
-    // @Override
-    // public ResponseEntity<List<ReporteDTO>> reportesPorId(Long id) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'reportesPorId'");
-    // }
 }
